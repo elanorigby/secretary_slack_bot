997eaab686c650cca5992b247b00703d522939f4
sharlak <sharla.kew@riversafe.co.uk>
2018-01-12 13:52:56 +0000

    removed extra functions, messages successfully logged to txt file

--- note_makr.py
+++ note_makr.py
@@ -11,11 +11,6 @@ slack_events_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, "/slack/event
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 CLIENT = SlackClient(SLACK_BOT_TOKEN)
 
-# inital message parse - enough to get subtype
-# if subtype, do nothing
-# if no subtype
-# extract message text, ts, channel, user
-# write to file
 
 @slack_events_adapter.on("message")
 def take_notes(event_data):
@@ -25,54 +20,14 @@ def take_notes(event_data):
         user = event["user"]
         channel = event["channel"]
         timestamp = event.get('ts')
-        
         file_name = "{}_{}notes.txt".format(user, channel)
-        content = "{} - {}".format(timestamp, message)
+        content = "[{} - {}]\n".format(timestamp, message)
         print(file_name)
         print(content)
         with open(file_name, 'a+') as file:
             file.write(content)
-
         reply = "It has been writen :memo:"
         CLIENT.api_call("chat.postMessage", channel=channel, text=reply)
-
-@slack_events_adapter.on("message")
-def affirm_notetaking(event_data):
-    message = event_data["event"]
-    message_text = message.get('text')
-    print(message_text)
-    if message.get('subtype') is None: 
-        if "take notes" in message_text:
-            channel = message["channel"]
-            print(channel)
-            reply = "Ok, <@{}>! I will begin taking notes :memo:".format(message["user"])
-            # print(reply)
-
-            CLIENT.api_call("chat.postMessage", channel=channel, text=reply)
-
-# # IF if message.get('subtype') is None 
-#@slack_events_adapter.on("message")
-#def store_notes(event_data):
-#    event = event_data["event"]
-#    file_name = "{}_{}.txt".format(message["user"], message["channel"])
-#    message_text = message.get('text')
-#    print(message_text)
-#    timestamp = message.get('ts')
-#    print(timestamp)
-#    with open(file_name, 'a+') as file:
-#        file.write(message_text, timestamp)
-
-
-# Example reaction emoji echo
-@slack_events_adapter.on("reaction_added")
-def reaction_added(event_data):
-    event = event_data["event"]
-    emoji = event["reaction"]
-    print(emoji)
-    channel = event["item"]["channel"]
-    text = ":{}:".format(emoji)
-    print(text)
-    CLIENT.api_call("chat.postMessage", channel=channel, text=text)
 
 
 # Once we have our event listeners configured, we can start the
