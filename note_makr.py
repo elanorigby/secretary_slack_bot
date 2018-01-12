1071179ee66dabc72b34b3b7f71c1ef19f65c27c
sharlak <sharla.kew@riversafe.co.uk>
2018-01-12 11:21:47 +0000

    subtype identification works

--- note_makr.py
+++ note_makr.py
@@ -11,27 +11,45 @@ slack_events_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, "/slack/event
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 CLIENT = SlackClient(SLACK_BOT_TOKEN)
 
+# inital message parse - enough to get subtype
+# if subtype, do nothing
+# if no subtype
+# extract message text, ts, channel, user
+# write to file
+
+@slack_events_adapter.on("message")
+def store_this(event_data):
+    event = event_data["event"]
+    if event.get('subtype') is None:
+        print("Subtype is none")
+        return True
+
 
 @slack_events_adapter.on("message")
 def affirm_notetaking(event_data):
     message = event_data["event"]
     message_text = message.get('text')
     print(message_text)
-    if message.get('subtype') is None and "take notes" in message_text:
-        channel = message["channel"]
-        print(channel)
-        reply = "Ok, <@{}>! I will begin taking notes :memo:".format(message["user"])
-        # print(reply)
-        CLIENT.api_call("chat.postMessage", channel=channel, text=reply)
+    if message.get('subtype') is None: 
+        if "take notes" in message_text:
+            channel = message["channel"]
+            print(channel)
+            reply = "Ok, <@{}>! I will begin taking notes :memo:".format(message["user"])
+            # print(reply)
+            CLIENT.api_call("chat.postMessage", channel=channel, text=reply)
 
-
-@slack_events_adapter.on("message")
-def store_notes(event_data):
-    message = event_data["event"]
-    file_name = "{}_{}.txt".format(message["user"], message["channel"])
-    with open(file_name, 'a+') as file:
-        file.write(message.get('text'))
-
+# IF if message.get('subtype') is None 
+#@slack_events_adapter.on("message")
+#def store_notes(event_data):
+#    message = event_data["event"]
+#    file_name = "{}_{}.txt".format(message["user"], message["channel"])
+#    message_text = message.get('text')
+#    print(message_text)
+#    timestamp = message.get('ts')
+#    print(timestamp)
+#    with open(file_name, 'a+') as file:
+#        file.write(message_text, timestamp)
+#
 
 # Example reaction emoji echo
 @slack_events_adapter.on("reaction_added")
